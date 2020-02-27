24d6eadfa405b8d91c53831879b25d44fbb58030
David Amunga <david.onyimbo@gmail.com>
2020-02-27 12:10:04 +0300

    Updates

---
+++ precache-manifest.5b4009409ce829577f101e6d967c7043.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d88209a80e4d0bb4489223f7752b47ad",
+    "url": "/git@github.com:DavidAmunga/react-editor-demo.git/index.html"
+  },
+  {
+    "revision": "0e4b9bb7f218d6fb680c",
+    "url": "/git@github.com:DavidAmunga/react-editor-demo.git/static/css/2.806183df.chunk.css"
+  },
+  {
+    "revision": "0e4b9bb7f218d6fb680c",
+    "url": "/git@github.com:DavidAmunga/react-editor-demo.git/static/js/2.8cab5cba.chunk.js"
+  },
+  {
+    "revision": "54eb23f04ed74d984a17656b3ab7eac2",
+    "url": "/git@github.com:DavidAmunga/react-editor-demo.git/static/js/2.8cab5cba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "193f12e68d0dabafd974",
+    "url": "/git@github.com:DavidAmunga/react-editor-demo.git/static/js/main.40942418.chunk.js"
+  },
+  {
+    "revision": "a448137238519bb87737",
+    "url": "/git@github.com:DavidAmunga/react-editor-demo.git/static/js/runtime-main.a69fdfca.js"
+  }
+]);
